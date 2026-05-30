9d389fcfab6d54d8c05706e0da0374cb905904ef
krawtschenko <eugenykravchenko@gmail.com>
2026-05-30 17:41:23 +0200

    feat: add component Dropdown

--- src/components/avatar/avatar.tsx
+++ src/components/avatar/avatar.tsx
@@ -6,14 +6,16 @@ import { UserRound } from 'lucide-react';
 
 import styles from './avatar.module.scss';
 
-type Props = { avatarSrc?: string } & ComponentPropsWithoutRef<typeof AvatarRadix.Root>;
+type Props = { avatarSrc?: string; size?: 'sm' | 'lg' } & ComponentPropsWithoutRef<
+  typeof AvatarRadix.Root
+>;
 
-export const Avatar = ({ className, avatarSrc, ...rest }: Props) => {
+export const Avatar = ({ className, avatarSrc, size = 'sm', ...rest }: Props) => {
   return (
-    <AvatarRadix.Root className={clsx(styles.avatar, className)} {...rest}>
+    <AvatarRadix.Root className={clsx(styles.avatar, styles[size], className)} {...rest}>
       <AvatarRadix.Image className={styles.img} src={avatarSrc} alt="avatar" />
       <AvatarRadix.Fallback className={styles.fallback}>
-        <UserRound size={18} />
+        <UserRound size={size === 'lg' ? 22 : 18} />
       </AvatarRadix.Fallback>
     </AvatarRadix.Root>
   );
